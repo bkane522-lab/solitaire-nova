edda86e7027862810eb37132624db1516bfbd31a
bkane522-lab <bkane522@gmail.com>
2026-06-21 23:02:03 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'solitaire-nova-v3-pro';
+const CACHE = 'solitaire-nova-v311-pro';
 const FILES = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./maskable-icon-512.png'];
 self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
